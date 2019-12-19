6c35f6b8f3002a42d80ca773c10c41d995c8251c
monique <monique1225aqui@gmail.com>
2019-12-19 12:32:16 -0300

    finalizado

---
+++ src/Appelt.java
@@ -0,0 +1,21 @@
+
+import java.applet.AudioClip;
+import java.net.URL;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moniq
+ */
+class Appelt {
+
+    static AudioClip newAudioClip(URL som) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
